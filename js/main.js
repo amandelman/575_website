b1be63429129812f463c4beb60f22e720a5f8cb1
amandelman <almondman@gmail.com>
2016-01-27 15:00:38 -0600

    Lesson deliverables and debug.js for module 2
    Lesson deliverables and debug.js for module 2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,117 @@
 // Javascript by Adam Mandelman, 2016
+
+
+// Populations of four cities I have lived in:
+// Port Vila, Vanuatu: 44,040
+// Byron Bay, Australia: 4,959
+// San Francisco, CA: 852,469
+// Madison, WI: 233,209
+
+
+//initialize function called when the script loads
+function initialize(){
+    cities();
+};
+
+//function to create a table with cities and their populations
+function cities(){
+    //define two arrays for cities and population
+    var cityPop = [
+        { 
+            city: 'Madison, Wisconsin',
+            population: 233209
+        },
+        {
+            city: 'San Francisco, California',
+            population: 852469
+        },
+        {
+            city: 'Byron Bay, Australia',
+            population: 4959
+        },
+        {
+            city: 'Port Vila, Vanuatu',
+            population: 44040
+        }
+    ];
+
+    //append the table element to the div
+    $("#mydiv").append("<table>");
+
+    //append a header row to the table
+    $("table").append("<tr>");
+
+    //add the "City" and "Population" columns to the header row
+    $("tr").append("<th>Cities in Which I've Lived</th><th>Population</th>");
+
+    //loop to add a new row for each city
+    for (var i = 0; i < cityPop.length; i++){
+        //assign longer html strings to a variable
+        var rowHtml = "<tr><td>" + cityPop[i].city + "</td><td>" + cityPop[i].population + "</td></tr>";
+        //add the row's html string to the table
+        $("table").append(rowHtml);
+    };
+};
+
+//call the initialize function when the document has loaded
+$(document).ready(initialize);
+
+
+//debug.js from module 2
+
+function addColumns(cityPop){
+    
+    $('tr').each(function(i){
+
+    	if (i == 0){
+
+    		$(this).apend('<th>City Size</th>');
+    	} else {
+
+    		var citySize;
+
+    		if (cityPop[i-1].population < 100000){
+    			citySize = 'Small';
+
+    		} else if (cityPop[i-1].population < 500000){
+    			citysize = 'Medium';
+
+    		} else {
+    			citySize = 'Large';
+    		};
+
+    		$this.append('<td' + citySize + '</td>');
+    	};
+    });
+};
+
+function addEvents(){
+
+	$('#table').mouseover(function(){
+		
+		var color = "rgb(";
+
+		for (var i=0; i<3; i++){
+
+			var random = Math.round(Math.random() * 255);
+
+			color += "random";
+
+			if (i<2){
+				color += ",";
+			
+			} else {
+				color += ")";
+		};
+
+		$(this).css('color', color);
+	});
+
+	function clickme(){
+
+		alert('Hey, you clicked me!');
+	};
+
+	$('table').on('click', clickme);
+};
+
